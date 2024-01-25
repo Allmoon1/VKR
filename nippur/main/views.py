3818f6945c1f0d807b49497f85d8ca77ca32b0ef
Allmoon <clarthetrue@gmail.com>
2024-01-25 12:12:10 +0300

    /

--- nippur/main/views.py
+++ nippur/main/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
-
 def login(request):
     return render(request, 'main/login.html')
 def register(request):
